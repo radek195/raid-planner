775f2d3d67b77bbdb52c7c9520333fadf71e41d7
Radek <radoslaw.parol95@gmail.com>
2024-10-08 17:54:37 +0200

    initial commit

---
+++ src/main/java/com/example/raid_planner/RaidPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.raid_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaidPlannerApplication.class, args);
+	}
+
+}
